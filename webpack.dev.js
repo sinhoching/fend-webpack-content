8ba35fb67af960246a4d80aa1d19ab6ee91138eb
Sin Ho Ching <sinhochingvictoria@gmail.com>
2020-06-24 12:43:46 -0700

    webpack-dev-server installed

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require ('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -19,6 +20,12 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
 }
